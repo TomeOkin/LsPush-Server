538749d113412be92183a224aa7c0f301d81ac47
tomeokin <tome.okin@gmail.com>
2016-09-07 23:18:02 +0800

    clean project

--- src/test/java/app/service/internal/WebPageMain.java
+++ src/test/java/app/service/internal/WebPageMain.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package app.service;
+package app.service.internal;
 
 import app.data.model.WebPageInfo;
 import app.data.parse.WebPageUtil;
